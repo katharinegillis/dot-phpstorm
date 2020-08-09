c3ed57c60ce25a4aa4785cdd9857ed9f67f1fcb9
Katie Cordes <gillis.katharine@gmail.com>
2020-08-08 18:52:14 -0700

    Add PhpStorm install

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+if ls /opt/PhpStorm-* 1> /dev/null 2>&1; then
+    echo "PhpStorm already installed"
+else
+    # Download PhpStorm standalone
+    wget https://download.jetbrains.com/webide/PhpStorm-2020.2.tar.gz
+
+    # Install PhpStorm
+    sudo tar -xzf PhpStorm-*.tar.gz -C /opt
+    sudo ln -s /opt/PhpStorm-202.6397.115/bin/phpstorm.sh /usr/local/bin/phpstorm
+
+    # Clean up
+    sudo rm -rf PhpStorm-2020.2.tar.gz
+fi
---
+++ uninstall.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Uninstall PhpStorm
+sudo rm -rf /usr/local/bin/phpstorm
+sudo rm -rf /opt/PhpStorm-202.6397.115
+sudo rm -rf ~/.config/JetBrains/PhpStorm*
+sudo rm -rf ~/.cache/Jetbrains/Phpstorm*
