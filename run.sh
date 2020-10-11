11a8f8d11651233d52760e8626cabdbf34500fe9
Katharine Cordes <katharinegillis@users.noreply.github.com>
2020-10-11 10:40:00 -0600

    Update to use snap on install

--- run.sh
+++ run.sh
@@ -1,15 +1,3 @@
 #!/usr/bin/env bash
 
-if ls /opt/PhpStorm-* 1> /dev/null 2>&1; then
-    echo "PhpStorm already installed"
-else
-    # Download PhpStorm standalone
-    wget https://download.jetbrains.com/webide/PhpStorm-2020.2.tar.gz
-
-    # Install PhpStorm
-    sudo tar -xzf PhpStorm-*.tar.gz -C /opt
-    sudo ln -s /opt/PhpStorm-202.6397.115/bin/phpstorm.sh /usr/local/bin/phpstorm
-
-    # Clean up
-    sudo rm -rf PhpStorm-2020.2.tar.gz
-fi
+sudo snap install phpstorm --classic
